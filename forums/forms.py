5d96592147b65bb61302747efab7888fc0678d5c
Monika Jakobsson <nimrin81@gmail.com>
2022-06-03 20:05:56 +0000

    Add topic buttons to prepare for forum view. Add style to visualize buttons. Create template for topic view and prepare for PostForm.

--- forums/forms.py
+++ forums/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Comments
+from .models import Comments, Post
 
 
 class CommentsForm(forms.ModelForm):
     class Meta:
         model = Comments
         fields = ('content',)
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'content',)
